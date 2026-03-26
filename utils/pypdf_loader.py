141813fd05385ae19e0ef2dc5fd63a4f97699ca5
awejCodes <108984247+awejofficial@users.noreply.github.com>
2026-03-26 15:25:39 +0530

    Delete utils directory

--- utils/pypdf_loader.py
+++
@@ -1,4 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-loader= PyPDFLoader('vi.pdf')
-docs=loader.load()
-print(len(docs))
--- utils/splitters.py
+++
@@ -1,9 +0,0 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-def split_documents(documents):
-    splitters = RecursiveCharacterTextSplitter(
-        chunk_size=3000,
-        chunk_overlap=200
-    )
-
-    return splitters.split_documents(documents)
