a8bcd4fc55a53e0de2829aed5793373a603da84b
awejCodes <108984247+awejofficial@users.noreply.github.com>
2026-03-27 14:38:51 +0530

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,9 @@
 import reflex as rx
+import os
 
 config = rx.Config(
     app_name="DOCU_AI",
+    backend_port=int(os.environ.get("PORT", 8000)),
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
